58d5df566ad18655a87da6263a0adc158d4f47c5
Elle Ongori <michelleongori@gmail.com>
2022-03-11 09:12:47 -0800

    program prints last digit of random no,

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,9 +25,9 @@ else if (last == 0)
 {
 	printf("Last digit of %d is %d and is 0\n", n, last);
 }
-else if (last < 6 && last != 0)
+else
 {
-	printf("Last digit of  %d is %d and is less that 6 and not 0\n", n, last);
+	printf("Last digit of %d is %d and is less that 6 and not 0\n", n, last);
 }
 
 return (0);
