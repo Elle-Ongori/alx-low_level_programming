6fa3b4d3f57dea8af877d7899720ba61159bcd2c
Elle Ongori <michelleongori@gmail.com>
2022-03-15 12:02:52 -0700

    print lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * Use _putchar only twice
+ * Return: 0
+ *
+ */
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+}
