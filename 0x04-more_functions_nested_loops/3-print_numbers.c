8725b297308716d4379d6c3eb3eafefcca925af2
Elle Ongori <michelleongori@gmail.com>
2022-03-17 08:58:01 -0700

    print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - is a function that prints num 0 to 9
+ *
+ * Return: always 0 (success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
