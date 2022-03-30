9fbf643269c01ca1c35cc7160916c0adf1cfa0fe
Elle Ongori <michelleongori@gmail.com>
2022-03-30 03:31:18 -0700

    puts and recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,7 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	_puts_recursion(using puts with recursion);
-
-	return (0);
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
 }
