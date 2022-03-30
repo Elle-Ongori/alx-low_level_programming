8e5557ffba13318e5a21f1ef6922c5cb6b860206
Elle Ongori <michelleongori@gmail.com>
2022-03-30 03:17:08 -0700

    recurcion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - write a string to stdout
+ * @s: is a string array
+ * Return: (0) Success
+ */
+
+void _puts_recursion(char *s)
+{
+	char s[] = "recursion with c";
+
+	_puts_recursion(s);
+
+	return (0);
+}
