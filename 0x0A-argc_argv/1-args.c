695f1b2ae2558b4c63896a10ebad6e538dbd550f
Elle Ongori <michelleongori@gmail.com>
2022-04-01 05:31:01 -0700

    print no. of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,13 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		(void) argv;
-		printf("%d\n", argc);
-	}
+	(void) argv;
+	printf("%d\n", argc);
 
 	return (0);
 }
