b2f44e253340832509479d5f65db5c553aab4073
ElleOngori <94163979+Elle-Ongori@users.noreply.github.com>
2022-04-05 19:13:19 +0300

    update _atoi

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,7 +1,9 @@
 #include <main.h>
 
 /**
- *
+ * _atoi: takes string value,  return int type
+ * @s: is a char type
+ * Return: (res)
  */
 
 int _atoi(char* s)
@@ -10,3 +12,6 @@ int _atoi(char* s)
 	
 	for (int i = 0; s[i] != '\0'; ++i)
 		res = res * 10 + s[i] - '0';
+	
+	return (res);
+}
